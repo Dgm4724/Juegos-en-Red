5879c339aa6f473d4a005b0aaf04ccdf0aeb7f44
Dgm4724 <149008527+Dgm4724@users.noreply.github.com>
2024-11-24 23:18:59 +0100

    Implemento los assets de los botones

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -4,39 +4,52 @@ class GameOverScene extends Phaser.Scene {
     }
   
     init(data) {
-      // Pasar la puntuación recibida desde GameScene
+      // Puntuacion recibida desde GameScene
       this.puntuacion = data.puntuacion || 0;  // Si no se pasa puntuación, asigna 0
     }
   
     create() {
       // Fondo
-      this.add.image(360, 240, "fondo1");
+      this.add.image(360, 240, "fondoGenerico");
   
-      // Texto de Fin del Juego
+      // Texto Game Over
       this.add.text(360, 120, "¡Fin del Juego!", {
         fontSize: "50px",
         color: "#ffffff",
       }).setOrigin(0.5);
   
-      // Texto con la puntuación obtenida
+      // Puntuacion
       this.add.text(360, 200, `Puntuación final: ${this.puntuacion}`, {
         fontSize: "30px",
         color: "#ffffff",
       }).setOrigin(0.5);
   
-      // Botón para volver al menú principal
-      const menuButton = this.add.image(360, 300, "menuButton").setInteractive();
-      menuButton.on("pointerdown", () => {
-        this.scene.start("MainMenuScene"); // Volver al menú principal
+      // Botón para volver al menu principal
+      const botonMenu = this.add.image(360, 300, "boton").setInteractive();
+      botonMenu.on("pointerdown", () => {
+        this.scene.start("MainMenuScene");
       });
+      botonMenu.setDisplaySize(250, botonMenu.height);
+
+      this.add.text(360, 300, "Volver al\nMenú Principal", {
+        fontSize: "24px",
+        color: "#ffffff",
+        align: 'center'
+      }).setOrigin(0.5);
   
       // Botón para reiniciar la partida
-      const restartButton = this.add.image(360, 380, "restartButton").setInteractive();
-      restartButton.on("pointerdown", () => {
-        this.scene.start("GameScene"); // Reiniciar el juego
+      const botonReinicio = this.add.image(360, 380, "boton").setInteractive();
+      botonReinicio.on("pointerdown", () => {
+        this.scene.start("GameScene");
       });
+      botonReinicio.setDisplaySize(250, botonReinicio.height);
+
+      this.add.text(360, 380, "Reiniciar", {
+        fontSize: "24px",
+        color: "#ffffff",
+        align: 'center'
+      }).setOrigin(0.5);
     }
   }
   
   export default GameOverScene;
-  
